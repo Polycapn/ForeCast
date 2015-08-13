39efb62a69e3b3055ba322231b789b62d7383f80
Polycap M. Nyakundi <Polycapn@gmail.com>
2015-08-13 10:16:24 -0500

    Working Weather APP
    ForeCast is an App I made while following Udacity's Sunshine course

---
+++ app/src/main/java/com/example/polycap/forecast/Sync/SunshineSyncService.java
@@ -0,0 +1,29 @@
+package com.example.polycap.forecast.Sync;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+/**
+ * Created by Polycap on 8/11/2015.
+ */
+public class SunshineSyncService extends Service {
+    private static final Object sSyncAdapterLock = new Object();
+    private static SunshineSyncAdapter sSunshineSyncAdapter = null;
+
+    @Override
+    public void onCreate() {
+        Log.d("SunshineSyncService", "onCreate - SunshineSyncService");
+        synchronized (sSyncAdapterLock) {
+            if (sSunshineSyncAdapter == null) {
+                sSunshineSyncAdapter = new SunshineSyncAdapter(getApplicationContext(), true);
+            }
+        }
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        return sSunshineSyncAdapter.getSyncAdapterBinder();
+    }
+}
